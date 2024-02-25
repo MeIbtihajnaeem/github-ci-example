1711a3f8b19a3dff38d81993f2274ca72a3b5051
ibtihajnaeem <i.ibtihajnaeem@gmail.com>
2024-02-26 00:03:55 +0100

    First workflow

---
+++ com/examples/myproject/src/test/java/com/examples/AppTest.java
@@ -0,0 +1,20 @@
+package com.examples;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+	private App app;
+
+	@Before
+	public void setup() {
+		app = new App();
+	}
+
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
+}
